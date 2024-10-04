f1069d047ecb2310f3832776f5754f974317faa5
George Croucher <gpcroucher@gmail.com>
2024-10-04 20:02:41 +0100

    added timestamp to Post component using date-fns

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,12 @@
+import { format } from "date-fns";
+
 export default function Post({ post }) {
   return (
     <div>
       <p>{post.title}</p>
       <p>{post.name}</p>
       <p>{post.message}</p>
+      <p>{format(post.created_at, "E do MMM y, kk:mm:ss")}</p>
     </div>
   );
 }
